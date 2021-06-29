fa93e7a27ca81d2f6d099ca6fc36db8e4d2df143
Ben Houston <neuralsoft@gmail.com>
2021-06-29 16:10:26 -0400

    make jest-puppeteer work.

--- api.test.js
+++ api.test.js
@@ -1,23 +1,8 @@
  const fetch = require('node-fetch');
- 
- let base64 = require('base-64');
-
-let url = 'http://eu.httpbin.org/basic-auth/user/passwd';
-let username = 'user';
-let password = 'passwd';
-
-let headers = new Headers();
-
-//headers.append('Content-Type', 'text/json');
-headers.append('Authorization', 'Basic ' + base64.encode(username + ":" + password));
-
 
 ['https://benhouston3d.com', 'https://threekit.com'].forEach( ( value )=> {
     test(`does ${value} response 200`, async () => {
-        const results = await fetch(value, {method:'GET',
-        headers: headers,
-        //credentials: 'user:passwd'
-       });
+        const results = await fetch(value);
         expect(results.status).toBe(200);
     });
 });
